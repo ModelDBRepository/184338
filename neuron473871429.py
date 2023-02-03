dc875831712cd3c66a7eb65bf4ac8f09a2c758f0
Rui Li <rui.li.rl895@yale.edu>
2023-02-03 11:32:44 -0500

    Update neuron473871429.py

--- neuron473871429.py
+++ neuron473871429.py
@@ -14,7 +14,8 @@ class Neuron473871429:
             
         Note: if name is not specified, Neuron473871429_instance is used instead
         '''
-        
+                
+        self._name = name
         # load the morphology
         from load_swc import load_swc
         load_swc('Nr5a1-Cre_Ai14_IVSCC_-177834.03.02.01_472399664_m.swc', self,
@@ -31,8 +32,7 @@ class Neuron473871429:
         self.axon[0].connect(self.soma[0](0.5))
         self.axon[1].connect(self.axon[0](1))
         self.all += self.axon
-        
-        self._name = name
+
         self._insert_mechanisms()
         self._discretize_model()
         self._set_mechanism_parameters()
